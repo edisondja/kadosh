2cf2878a9e97f9d6efe0c85959881c7aab0e13d7
=MisterED <=edisondja@gmail.com>
2019-12-28 14:54:52 -0400

    Ultra cambios

--- src/components/factura_interfaz.js
+++ src/components/factura_interfaz.js
@@ -2,19 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Axios from 'axios';
 import Alertify from 'alertifyjs';
+import ReactPrint from 'react-to-print';
+import ImprimirRecibo from './imprimir_recibo';
 import '../css/dashboard.css';
 
 class FacturaInterfaz extends React.Component{
 
     constructor(props){
         super(props);
-        this.state= {valor:true,factura:{precio_estatus:0},procedimientos:[]};
+        this.state={procedimientos_imprimir:[],config:'normal',data_factura:[],valor:true,factura:{precio_estatus:0,id:0},procedimientos:[],total:0,recibos:[],mensaje:""};
     }
 
     componentDidMount(){
 
         this.cargar_factura(this.props.id_factura);
         this.cargar_procedimientos(this.props.id_factura);
+        this.cargar_recibos(this.props.id_factura);
     }
 
     cargar_procedimientos=(id_factura)=>{
@@ -28,6 +31,103 @@ class FacturaInterfaz extends React.Component{
         });
 
     }
+    
+    cargar_recibos=(id_factura)=>{
+
+        Axios.get(`http://localhost:8000/api/cargar_recibos/${id_factura}`).then(data=>{
+                this.setState({recibos:data.data});
+        }).catch(error=>{
+                Alertify.error("error al cargar los recibos");
+        });
+
+    }
+    
+    descontar(id_factura){
+            
+        Alertify.prompt("Descuento manual","Digite la cantidad que le quiere descontar a la facutra.","",function(event,value){
+
+                alert(value);
+
+        },function(error){
+
+            Alertify.message("Descuento cancelado...");
+        });
+        
+    }
+
+    imprimir_factura=(id_recibo,id_factura)=>{
+            
+        Axios.get(`http://localhost:8000//api/imprimir_recibo/${id_recibo}/${id_factura}`).then(data=>{
+                this.setState({config:'imprimir_recibo',data_factura:data.data.recibo,procedimientos_imprimir:data.data.procedimientos});
+        }).catch(error=>{
+                Alertify.error("No se pudo imprimir el recibo");
+        });
+
+    }
+
+    actualizar_recibo(id_recibo){
+        
+            Alertify.prompt("Autenticación","Para mayor seguiridad y poder tener acceso a editar el recibo digite su contraseña de administracion en este campo.","",function(event,value){
+
+                alert(value);
+
+            },function(error){
+
+            }).set("type","password");
+
+    }
+
+    eliminar_recibo(id_recibo,id_factura){
+
+        Alertify.confirm("Eliminar Recibo","Estas seguro que quieres eliminar este recibo?",function(){
+                    Axios.get(`http://localhost:8000/api/eliminar_recibo/${id_recibo}/${id_factura}`).then(data=>{
+                            Alertify.success("Recibo eliminado correctamente..");
+                    }).catch(error=>{
+                            Alertify.error("No se pudo eliminar el recibo..");
+                    });
+        },function(){
+                Alertify.message("BYE");
+        });
+    }
+
+    procesar_pago=(id_factura,precio_estatus)=>{
+
+            Alertify.confirm("Kadosh","Deseas realizar un pago de esta factura?",function(){
+                            
+                    Alertify.prompt("Pagando factura","<select id='seleccionar_pago'><option value='1'>EFECTIVO</option><option value='2'>TARGETA</option><option value='3'>CHEQUE</option></select> <p>Seleccione el tipo de pago</button>","$RD 00.00",function(event,value){
+                        
+                        let option = document.getElementById("seleccionar_pago").value;
+
+                        if(option==1){
+
+                            Axios.get(`http://localhost:8000/api/pagar_recibo/${id_factura}/${value}/efectivo/${precio_estatus}`).then(data=>{
+                                            Alertify.success("Pago realizado con exito");
+                                            this.setState({mensaje:data});
+                            }).catch(error=>{
+                                  Alertify.error("No se pudo procesar el pago correctamente");
+                            });
+                            
+                        }else if(option==2){
+                            Alertify.message("has seleccionado pago en targeta");
+
+                        }else if(option==3){
+                            Alertify.message("has seleccionado pago para cheques");
+                        }else{
+                            
+                            Alertify.error("Numeracion de pago incorrecta");
+                        }
+                        
+ 
+                    },function(){
+                            Alertify.message("Pago cancelado");
+                    }).set('type','text');
+
+            },function(){
+
+            });
+
+
+    }
 
     cargar_factura=(id_factura)=>{
             
@@ -39,7 +139,13 @@ class FacturaInterfaz extends React.Component{
     }   
 
     render(){
+        if(this.state.config=='imprimir_recibo'){
+        
+               return  <ImprimirRecibo data_recibo={this.state.data_factura} procedimientos_i={this.state.procedimientos_imprimir}/>
+        }
+
         return (<div className="col-md-4"> 
+          
                 <h2>Factura y sus detalles</h2>
                 <h3>Estado actual <p style={{color:'#36b836'}}>$RD {this.state.factura.precio_estatus}</p></h3><hr/>
                 <h4>Ingresado por: ({this.state.factura.nombre} {this.state.factura.apellido})</h4>
@@ -56,13 +162,14 @@ class FacturaInterfaz extends React.Component{
                                 <td>{data.nombre}</td>
                                 <td>{data.cantidad}</td>
                                 <td>$RD {data.total}</td>
+                                <td style={{display:'none'}}>{this.state.total+= data.total/2} </td>
                             </tr>
                       )))     
                     }
                     </table>
-                    <button className="btn btn-primary">Pagar</button>&nbsp;
-                    <button className="btn btn-info">Imprimir</button>&nbsp;
-                    <button className="btn btn-dark">Eliminar</button><hr/>
+                    <button className="btn btn-success" onClick={()=>this.procesar_pago(this.props.id_factura,this.state.factura.precio_estatus)}>Pagar</button>&nbsp;
+                    <button className="btn btn-primary" onclick={()=>this.desconctar(this.props.id_factura)}>Descontar</button>&nbsp;
+                    <button className="btn btn-dark">X</button><strong>&nbsp;&nbsp;&nbsp;TOTAL $RD {this.state.total}</strong> <hr/>
                     <h2>Pagos Realizados</h2>
                     <table className="table boxslider">
                         <tr>
@@ -73,14 +180,21 @@ class FacturaInterfaz extends React.Component{
                             <td>Eliminar</td>
                             <td>Imprimir</td>
                         </tr>
-                            <tr>
-                                <td>400</td>
-                                <td>pago en efectivo</td>
-                                <td>MASTER CARD</td>
-                                <td><button className="btn btn-primary">Actualizar</button></td>
-                                <td><button className="btn btn-success">Eliminar</button></td>
-                                <td><button class="btn btn">Imprimir</button></td>
-                            </tr> 
+                           {
+                               this.state.recibos.map((data=>(
+
+                                    <tr>
+                                        <td>{data.monto}</td>
+                                        <td>{data.concepto_pago}</td>
+                                        <td>{data.tipo_de_pago}</td>
+                                        <td><button className="btn btn-primary" onClick={()=>this.actualizar_recibo(data.id)}>Actualizar</button></td>
+                                        <td><button className="btn btn-success" onClick={()=>this.eliminar_recibo(data.id,this.props.id_factura)}>Eliminar</button></td>
+                                        <td><button class="btn btn" onClick={()=>this.imprimir_factura(data.id,this.props.id_factura)}>Imprimir</button></td>
+                                    </tr> 
+
+                               )))
+
+                           }
                             
                     </table>
                 </div>
---
+++ src/components/imprimir_recibo.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import Axios from 'axios';
+import '../css/dashboard.css';
+class ImprimirRecibo extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state= {monto_total:0};
+
+    }
+
+    componentDidMount(){
+    }
+
+    Imprimir(){
+        
+            var ficha = document.getElementById("recibo");
+            var ventimp = window.open(' ', 'popimpr');
+            ventimp.document.write( ficha.innerHTML );
+            ventimp.document.close();
+            ventimp.print( );
+            ventimp.close();
+
+    }
+
+    render(){
+        
+        return (<div><hr/>
+                    <div className="card" id="recibo">
+                            <h4>Reicibo #{this.props.data_recibo.id}</h4>
+                                <strong className="titulo_kadosh">CLINICA DENTAL KADOSH</strong>
+                                <p>RL C/San Antonio #33A Los Alcarrizos Santo Domingo,R.D TEL: 809-620-8641 RNC: 131-76629-3</p>
+                                <table className="table">
+                                    <tr>
+                                        <td>Procedimiento</td>
+                                        <td>Precio</td>
+                                        <td>Cantidad</td>
+                                        <td>Total</td>
+                                    </tr>
+                                
+                                        {
+                                            this.props.procedimientos_i.map((data=>( 
+                                                <tr>
+                                                    <td>{data.nombre}</td>
+                                                    <td>{data.precio}</td>
+                                                    <td>{data.cantidad}</td>
+                                                    <td>{data.total}</td>
+                                                    <td style={{display:'none'}}><strong>{this.state.monto_total+=data.total}</strong></td>
+                                                </tr>
+                                            )))
+
+                                        }
+                                    <tr>
+                                        <td><strong>TOTAL</strong></td>
+                                        <td><strong>{this.state.monto_total}</strong></td>
+                                    </tr>
+                                </table><hr/>
+                                <table className="table">
+                                    <tr>
+                                        <td>Doctor</td>
+                                        <td>Paciente</td>
+                                        <td>Monto Pagado</td>
+                                        <td>Resto a pagar</td>
+                                    </tr>
+                                    <tr><td>{this.props.data_recibo.nombre} {this.props.data_recibo.apellido}</td>
+                                        <td>{this.props.data_recibo.paciente} {this.props.data_recibo.apellido_paciente}</td>
+                                        <td>RD$ {new Intl.NumberFormat().format(this.props.data_recibo.monto)}</td>
+                                        <td>RD$ {new Intl.NumberFormat().format(this.props.data_recibo.estado_actual)}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Concepto de pago</td>
+                                        <td>Tipo de pago</td>
+                                        <td>RNC</td>
+                                        <td>Fecha de pago</td>
+                                    </tr>
+                                    <tr>
+                                        <td>ABONO</td>
+                                        <td>EFECTIVO</td>
+                                        <td>5616</td>
+                                        <td>{this.props.data_recibo.created_at}</td>
+                                    </tr>
+                                   
+                                </table>    
+
+                                <strong>Ref: {this.props.data_recibo.id_factura}</strong>
+                                <strong style={{float:'rigth'}}>   Firma _____________________</strong>
+                    </div> 
+                    <button className="btn btn-primary" onClick={this.Imprimir}>Imprimir</button>
+
+            </div>);
+
+    }
+
+
+}
+
+export default ImprimirRecibo;
